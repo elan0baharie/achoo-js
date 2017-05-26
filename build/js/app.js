67dad2f1da546e987534a5de8eab50a7f6dee10a
Elan Baharie <0elan1@gmail.com>
2017-05-26 11:00:36 -0800

    development server is up, front end is getting info from get request

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "9e08fa12e79745b8b243b4d89f2e0838"
+
+},{}],2:[function(require,module,exports){
 function Search(userSearch){
   this.search = userSearch
 }
@@ -9,17 +12,24 @@ Search.prototype.condition = function(){
 
 exports.searchModule = Search
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 var Search = require('./../js/user-search.js').searchModule;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $('#searchForm').submit(function(e){
     e.preventDefault();
-    var userSearch= $('#userTxt').val();
-    var newSearch = new Search(userSearch);
-    newSearch.condition();
-    console.log(newSearch['search']);
+    var medicalIssue = $('#userTxt').val();
+
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(result) {
+        console.log(JSON.stringify(result));
+      });
+
+
+
+
+
   });
 })
 
-},{"./../js/user-search.js":1}]},{},[2]);
+},{"./../.env":1,"./../js/user-search.js":2}]},{},[3]);
--- js/search-interface.js
+++ js/search-interface.js
@@ -1,11 +1,18 @@
 var Search = require('./../js/user-search.js').searchModule;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $('#searchForm').submit(function(e){
     e.preventDefault();
-    var userSearch= $('#userTxt').val();
-    var newSearch = new Search(userSearch);
-    newSearch.condition();
-    console.log(newSearch['search']);
+    var medicalIssue = $('#userTxt').val();
+
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey, function(result) {
+        console.log(JSON.stringify(result));
+      });
+
+
+
+
+
   });
 })
